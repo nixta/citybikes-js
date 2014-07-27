a2d101436205493c2c213602a14b9dd09ddecd6c
Nicholas Furness <nfurness@nixta.com>
2014-07-27 00:23:30 -0400

    Consolidate Caching model for Networks and Stations

--- citybikes.js
+++ citybikes.js
@@ -5,10 +5,12 @@ var baseURL = 'http://api.citybik.es',
     networksURL = baseURL + '/v2/networks',
     networkURLBase = networksURL + '/';
 
-var networksCache = {}, stationsCache = {},
-    nextNetworksRefreshDue = new Date(),
+var networksCache, 
+    stationsCache,
     cacheValidityPeriodForNetworks = 60*60*1000, // 60 minutes for network lists
     cacheValidityPeriodForStations = 1*60*1000; // 1 minute for stations
+
+clearCaches();
 
 function geoJSONify(item) {
   return { "type": "Feature",
@@ -17,22 +19,33 @@ function geoJSONify(item) {
            "id": item.id };
 }
 
-function isCacheInvalid() {
-  return _.size(networksCache) == 0 || new Date() >= nextNetworksRefreshDue;
+function clearCaches() {
+    networksCache = {
+      data: {},
+      cacheExpiration: new Date()
+    }; 
+    stationsCache = {
+      data: {},
+      cacheExpiration: new Date()
+    };  
 }
 
-exports.clearCache = function() {
-  networksCache = {};
-  stationsCache = {};
-  nextNetworksRefreshDue = new Date();
+function isCacheInvalid(cache) {
+  return _.size(cache.data) == 0 || new Date() >= cache.cacheExpiration;
+}
+
+exports.clearCaches = function() {
+  clearCaches();
 }
 
 exports.networks = function(callback) {
-  if (isCacheInvalid()) {
+  if (isCacheInvalid(networksCache)) {
     request(networksURL, function(error, response, body) {
       if (!error && response.statusCode == 200) {
-        nextNetworksRefreshDue = new Date((new Date()).getTime() + cacheValidityPeriodForNetworks);
-        networksCache = _.indexBy(_.map(JSON.parse(body).networks, geoJSONify), 'id');
+        networksCache = {
+          data: _.indexBy(_.map(JSON.parse(body).networks, geoJSONify), 'id'),
+          cacheExpiration: new Date((new Date()).getTime() + cacheValidityPeriodForNetworks)
+        };
         return callback(null, processOutput(networksCache));
       } else {
         return callback(error, null);
@@ -43,7 +56,7 @@ exports.networks = function(callback) {
   }
 
   function processOutput(output) {
-    return _.mapValues(_.cloneDeep(output), function(n) { 
+    return _.mapValues(_.cloneDeep(output.data), function(n) { 
       n.properties = _.omit(n.properties, ['id', 'href']); 
       return n; 
     });
@@ -54,7 +67,7 @@ exports.network = function(networkName, callback) {
   // Return the cache if present and valid.
   if (stationsCache.hasOwnProperty(networkName)) {
     var cacheEntry = stationsCache[networkName];
-    if (new Date() < cacheEntry.cacheExpiration) {
+    if (!isCacheInvalid(cacheEntry)) {
       return callback(null, processOutput(cacheEntry));
     }
   }
@@ -62,9 +75,9 @@ exports.network = function(networkName, callback) {
   // We didn't have a valid cache to return.
   // Get a URL for the network, but read it from the networks list if possible.
   var networkURL = '';
-  if (networksCache.hasOwnProperty(networkName)) {
-    var cacheEntry = networksCache[networkName];
-    networkURL = baseURL + cacheEntry.properties.href;
+  if (networksCache.data.hasOwnProperty(networkName)) {
+    var networkEntry = networksCache.data[networkName];
+    networkURL = baseURL + networkEntry.properties.href;
   } else {
      networkURL = networkURLBase + networkName;
   }
